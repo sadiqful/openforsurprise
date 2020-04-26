a2f2f0026209dbfb98ea9915d980ebefc04d77a3
Aliyu Abubakar <sadiqful@gmail.com>
2020-04-26 22:24:54 +0100

    checking for condition before animating

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ words[arrayCurrent].style.opacity = 1;
 for (var i = 0; i < words.length; i++) {
   spliLetters(words[i]);
 }
+
+function changeWord() {
+  var initialWord = arrayOfWord[arrayCurrent];
+  var newArray = arrayCurrent == words.length - 1 ? arrayOfWord[0] : arrayOfWord[arrayCurrent+1];
+  for (var i = 0; i < initialWord.length; i++) {
+    animateLetterOut(initialWord, i)
+  }
+}
