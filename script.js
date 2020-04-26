4ec76a0eb88b0de85b45775533c61ce6961e469b
Aliyu Abubakar <sadiqful@gmail.com>
2020-04-26 22:20:24 +0100

    selected word

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 var words = document.getElementsByClassName('word');
 var arrayOfWord = [];
 var arrayCurrent = 0;
+
+words[arrayCurrent].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  spliLetters(words[i]);
+}
